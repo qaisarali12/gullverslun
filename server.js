27de8ce6203efb5e1e699c756ea3268fc39c6c40
qaisarali12 <qaisaralishopify@gmail.com>
2026-01-11 19:41:19 +0500

    Force SNI and IPv4

--- server.js
+++ server.js
@@ -106,5 +106,5 @@ app.post("/api/check-auth-status", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log(`✅ Server running on port: ${PORT}`);
 });
