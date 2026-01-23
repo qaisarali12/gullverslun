ac9bd0c052a4766668c62cb02cfb7b079ea8f7ff
qaisarali12 <136271461+qaisarali12@users.noreply.github.com>
2026-01-23 22:16:50 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const https = require("https");
+const crypto = require("crypto"); // Added for password generation
 const app = express();
 
 app.use(cors());
